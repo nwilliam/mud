9fde6a06116f3a472b82d806a9e3c91a7e30adf3
Nate Williams <nwilliams.fly@gmail.com>
2015-11-07 11:03:49 -0600

    Created BaseObject class.  Added Unit Tests for BaseObject class.

--- NateMUD/model/baseobject.py
+++ NateMUD/model/baseobject.py
@@ -10,8 +10,50 @@ class BaseObject(object):
     '''
 
 
-    def __init__(self, noun='rock', adjs='very large'):
-        '''
-        Constructor
-        '''
+    def __init__(self, noun='rock', adjs='very large', 
+                 article=None, short=None, desc=None, 
+                 isPlural=False, **kwargs):
+        self.noun = noun
+        self.adjs = adjs.split(' ')
+        self.article = article
+        self.short = short
+        self.desc = desc
+        self.isPlural = isPlural
+    
+    def Adjs(self):
+        if self.adjs:
+            return self.adjs
+        else:
+            return ''
+
+    def AShort(self):
+        article = self.Article()
+        short = self.Short()
+        if article:
+            return '%s %s' % (article,short)
+        else:
+            return short
+    
+    def Article(self):
+        if self.article:
+            return self.article
+        elif self.isPlural:
+            return 'some'
+        else:
+            return 'an' if self.Short()[0] in 'aeiou' else 'a'
+       
+    def Short(self):
+        if self.short:
+            return self.short
+        else:
+            return ' '.join([' '.join(self.adjs),self.noun]).strip()
+    
+    def Desc(self):
+        if self.desc:
+            return self.desc
+        else:
+            return 'This is %s' % (self.AShort())
+    
+
+        
         
---
+++ NateMUD/tests/model_test.py
@@ -0,0 +1,51 @@
+'''
+Created on Nov 7, 2015
+
+@author: nwilliams
+'''
+import unittest
+
+from model.baseobject import BaseObject
+
+class TestBaseObject(unittest.TestCase):
+
+    def testBaseObjectShortWithNoAdjs(self):
+        obj = BaseObject(noun='stone',adjs='')
+        self.assertEqual(obj.Short(), 'stone')
+    
+    def testBaseObjectShortWithOneAdj(self):
+        obj = BaseObject(noun='stone',adjs='small')
+        self.assertEqual(obj.Short(),'small stone')
+        
+    def testBaseObjectShortWithMultipleAdjs(self):
+        obj = BaseObject(noun='stone',adjs='small pointy crazy drunken')
+        self.assertEqual(obj.Short(),'small pointy crazy drunken stone')
+    
+    def testBaseObjectAShortWithNoAdjsConsonant(self):
+        obj = BaseObject(noun='stone',adjs='')
+        self.assertEqual(obj.AShort(),'a stone')    
+
+    def testBaseObjectAShortWithAdjsConsonant(self):
+        obj = BaseObject(noun='stone',adjs='small pointy')
+        self.assertEqual(obj.AShort(),'a small pointy stone')
+        
+    def testBaseObjectAShortWithNoAdjsVowel(self):
+        obj = BaseObject(noun='apple',adjs='')
+        self.assertEqual(obj.AShort(),'an apple')        
+
+    def testBaseObjectAShortWithAdjsVowel(self):
+        obj = BaseObject(noun='apple',adjs='small')
+        self.assertEqual(obj.AShort(),'a small apple')
+    
+    def testBaseObjectAShortWithVowelAdjsVowel(self):
+        obj = BaseObject(noun='apple',adjs='alluring green')
+        self.assertEqual(obj.AShort(),'an alluring green apple')    
+
+    def testBaseObjectAShortWithPlural(self):
+        obj = BaseObject(noun='apples',adjs='alluring green',isPlural=True)
+        self.assertEqual(obj.AShort(),'some alluring green apples') 
+      
+         
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
