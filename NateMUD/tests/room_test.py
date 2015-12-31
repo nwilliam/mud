71706ba261cdf146c3ca670a5c0d02e975235d95
nwilliams <nwilliams@nwilliams-t440s.xirgotech.local>
2015-12-31 10:56:32 -0600

    Split Room and BaseObject tests into separate files.  These are going to get large.

--- NateMUD/tests/room_test.py
+++ NateMUD/tests/room_test.py
@@ -1,71 +1,12 @@
 '''
-Created on Nov 7, 2015
+Created on Dec 31, 2015
 
 @author: nwilliams
 '''
 import unittest
-
 from models.baseobject import BaseObject
 from models.body import Body
 from models.room import Room
-
-class TestBaseObject(unittest.TestCase):
-
-    def testBaseObjectShortWithNoAdjs(self):
-        obj = BaseObject(noun='stone',adjs='')
-        self.assertEqual(obj.Short(), 'stone')
-    
-    def testBaseObjectShortWithOneAdj(self):
-        obj = BaseObject(noun='stone',adjs='small')
-        self.assertEqual(obj.Short(),'small stone')
-        
-    def testBaseObjectShortWithMultipleAdjs(self):
-        obj = BaseObject(noun='stone',adjs='small pointy crazy drunken')
-        self.assertEqual(obj.Short(),'small pointy crazy drunken stone')
-    
-    def testBaseObjectAShortWithNoAdjsConsonant(self):
-        obj = BaseObject(noun='stone',adjs='')
-        self.assertEqual(obj.AShort(),'a stone')    
-
-    def testBaseObjectAShortWithAdjsConsonant(self):
-        obj = BaseObject(noun='stone',adjs='small pointy')
-        self.assertEqual(obj.AShort(),'a small pointy stone')
-        
-    def testBaseObjectAShortWithNoAdjsVowel(self):
-        obj = BaseObject(noun='apple',adjs='')
-        self.assertEqual(obj.AShort(),'an apple')        
-
-    def testBaseObjectAShortWithAdjsVowel(self):
-        obj = BaseObject(noun='apple',adjs='small')
-        self.assertEqual(obj.AShort(),'a small apple')
-    
-    def testBaseObjectAShortWithVowelAdjsVowel(self):
-        obj = BaseObject(noun='apple',adjs='alluring green')
-        self.assertEqual(obj.AShort(),'an alluring green apple')    
-
-    def testBaseObjectAShortWithPlural(self):
-        obj = BaseObject(noun='apples',adjs='alluring green',isPlural=True)
-        self.assertEqual(obj.AShort(),'some alluring green apples') 
-      
-    def testBaseObjectFullMultipleAdjs(self):
-        obj = BaseObject('tiny wet stone')
-        self.assertEqual(obj.AShort(),'a tiny wet stone')
-
-    def testBaseObjectFullOneAdjs(self):
-        obj = BaseObject('wet stone')
-        self.assertEqual(obj.AShort(),'a wet stone')
-        
-    def testBaseObjectFullNoAdjs(self):
-        obj = BaseObject('stone')
-        self.assertEqual(obj.AShort(),'a stone')
-    
-    def testBaseObjectisaBaseObject(self):
-        obj = BaseObject('stone')
-        self.assertEqual(obj.isa('baseobject'),True)
-        
-    def testBaseObjectisaBody(self):
-        obj = BaseObject('stone')
-        self.assertEqual(obj.isa('body'), False)        
 
 class TestRooms(unittest.TestCase):
     def testRoomAddToContentsObject(self):
@@ -136,6 +77,7 @@ class TestRooms(unittest.TestCase):
         room = Room()
         self.assertEqual(room.isa('body'),False)   
                     
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
