d522ff02f31d0d6d2d0eb5a3d0ce9a62cff5c9bf
nwilliams <nwilliams@nwilliams-t440s.xirgotech.local>
2015-12-31 10:52:45 -0600

    Created Being class for "Sentient" things -> NPCs, Players, etc.
    Tore all of the Being stuff out of Body.  Body should be a top level
    class with only things like Location/Room.

---
+++ NateMUD/models/being.py
@@ -0,0 +1,45 @@
+'''
+Created on Dec 31, 2015
+
+@author: nwilliams
+'''
+from models.body import Body
+
+class Being(Body):
+    '''
+    Body is anything - a spider, a monkey, a human, an elf.  Anything living.
+    Being is a humanoid (NPC/Player).  It has a name, a title, lots of fun crap.
+    
+    '''
+    def __init__(self, name='unnamed',pretitle='',posttitle='',**kwargs):
+        super(Being,self).__init__(noun=name.title(),adjs='', length=20, 
+                          width=15, height=72, weight=160, **kwargs)
+        self.name = name.title()
+        self.pretitle = pretitle.title()
+        self.posttitle = posttitle.title()
+
+        
+    def Name(self):
+        '''
+        Name if you have it, else Noun if you have it, if not then just Unnamed.
+        '''
+        if self.name:
+            return self.name
+        else:
+            if self.noun:
+                return self.noun
+            else:
+                return 'Unnamed'
+    
+    def Noun(self):
+        return self.Name()
+            
+    def Article(self):
+        #Beings don't get articles.
+        return ''
+    
+    def FullName(self):
+        if self.posttitle.startswith(','):
+            return '%s %s%s' % (self.pretitle,self.Name(),self.posttitle)
+        else:
+            return '%s %s %s' % (self.pretitle,self.Name(),self.posttitle)
--- NateMUD/models/body.py
+++ NateMUD/models/body.py
@@ -2,6 +2,36 @@
 Created on Nov 7, 2015
 
 @author: nwilliams
+    
+    A Body is a Mobile Object (MOB) or a Player.
+    
+    Should every Body have a client? Yes I think so.  This lets me
+    have the ability to possess non-player objects and have them DoStuff.
+    There's a lot of things that will be added to a Body, but for
+    now we'll just keep the basics and template the rest.
+    
+    I think I should branch this into subclasses. 
+    Body -> Being -> Player
+    Body -> Mob
+    
+    Everything has the ability to have a Client.  Beings will 
+    have pretitles and post titles.  How titilating.  Do NPCs and Players have
+    anything different?  I think only scripts.  I take that back, I want the
+    ability to have environmental scripts on players.
+    
+    Is there anything different?  Yes, there is.  Persist().  
+    
+    I think mobs may need to be a subclass for movement and because they don't
+    have names.
+    
+    I'm going to keep self.client in Body.  I want to be able to possess mobs.
+    
+    I think I want to treat Bodies like BaseObjects.  That way I can call
+    all of the BaseObject methods on it if I need to.  I'll just override
+    most of them as needed.
+    
+    All Bodies are Containers.  I'll do this as a Mixin.
+    
 '''
 from models.baseobject import BaseObject
 from models.container import Container
@@ -14,46 +44,11 @@ class Body(BaseObject, Container):
     client=None,name='unnamed',pretitle='',posttitle='',
     desc_string='',location=None,**kwargs
     '''
-    '''
-    A Body is a Mobile Object (MOB) or a Player.
-    
-    Should every Body have a client? Yes I think so.  This lets me
-    have the ability to possess non-player objects and have them DoStuff.
-    There's a lot of things that will be added to a Body, but for
-    now we'll just keep the basics and template the rest.
-    
-    I think I should branch this into subclasses. 
-    Body -> Being -> Player
-    Body -> Mob
-    
-    NPC's and Players have names and the ability to have a Client.  They also
-    have pretitles and post titles.  How titilating.  Do NPCs and Players have
-    anything different?  I think only scripts.  I take that back, I want the
-    ability to have environmental scripts on players.
-    
-    Is there anything different?  Yes, there is.  Persist().  
-    
-    I think mobs may need to be a subclass for movement and because they don't
-    have names.
-    
-    I'm going to keep self.client in Body.  I want to be able to possess Mobs.
-    
-    I think I want to treat Bodies like BaseObjects.  That way I can call
-    all of the BaseObject methods on it if I need to.  I'll just override
-    most of them.
-    
-    Bodies are also Containers.  I'll do this as a Mixin.
-    '''
 
-
-    def __init__(self, client=None,name='unnamed',pretitle='',posttitle='',
-                 desc_string='',location='staff/default/000000',**kwargs):
-        super(Body,self).__init__(noun=name.title(),adjs='', length=20, 
-                                  width=15, height=72, weight=160, **kwargs)
+    def __init__(self, client=None,desc_string='',
+                 location='staff/default/000000',**kwargs):
+        super(Body,self).__init__(length=20,width=15, height=72, weight=160, **kwargs)
         self.client = client
-        self.name = name.title()
-        self.pretitle = pretitle.title()
-        self.posttitle = posttitle.title()
         self.desc_string = desc_string
         self.location = location #Location needs to be stored as an address. :\
         self.room = None
@@ -76,28 +71,6 @@ class Body(BaseObject, Container):
     
     def Unpossess(self,client):
         self.client = None
-    
-    def Name(self):
-        if self.name:
-            return self.name
-        else:
-            return 'Unnamed'
-    
-    def Noun(self):
-        if self.name:
-            return self.name
-        else:
-            return 'Unnamed'
-        
-    def Article(self):
-        #Bodies don't get articles.
-        return ''
-    
-    def FullName(self):
-        if self.posttitle.startswith(','):
-            return '%s %s%s' % (self.pretitle,self.Name(),self.posttitle)
-        else:
-            return '%s %s %s' % (self.pretitle,self.Name(),self.posttitle)
         
     def Tell(self,msg,**kwargs):
         # I'm abstracting this a bit higher to reduce calls to
